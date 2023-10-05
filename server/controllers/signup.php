a9985746d2fa03a40857a8f93455abcae1b7a6c4
christodharma <13521009@std.stei.itb.ac.id>
2023-10-05 15:34:55 +0700

    feat: signin_query.php for querying user and admin feat: authentication for user and admin fix: signup to check existence on server side

--- server/controllers/signup.php
+++ server/controllers/signup.php
@@ -1,59 +1,34 @@
 <?php 
     require_once "connect_database.php";
+    require_once "signin_query.php";
     //only accepting post method
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        //verification process
-        $conn = connect_database();
-        $query = "SELECT * FROM User";
-        $stmt = $conn->prepare($query);
-        
-
-        if (!$stmt) {
-            die("Error in query preparation: " . $conn->error);
-        }
-
-        $result = $stmt->execute();
-
-        if (!$result) {
-            die("Error in query execution: " . $stmt->error);
-        }
-        // Bind the result variables
-        // $stmt->bind_result($resultEmail, $resultUsername, $resultPassword);
-        $resultSet = $stmt->get_result();
-        // Fetch the result
-        // $stmt->fetch();
-        $rows = $resultSet->fetch_all();
-
-        // If found something from the query, determined by the fetched results
-        // and making response as json format
-        if (!empty($rows)) {
-            $response = array("success" => true, "message" => "data sent", "data" => $rows);
-        } else {
-            $response = array("success" => false, "message" => "Error: not found");
-        }
-        echo json_encode($response);
-        mysqli_close($conn);
-    } else if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        $conn = connect_database();
-        $query = "INSERT INTO `User` (`email`, `username`, `password`) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($query);
-
-        if (!$stmt) {
-            die("Error in query preparation: " . $conn->error);
-        }
-
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
         $email    = $_POST["email"];
         $username = $_POST["username"];
         $password = $_POST["password"];
-        $stmt->bind_param("sss", $email, $username, $password);
-        $result = $stmt->execute();
+        $conn = connect_database();
 
-        if (!$result) {
-            die("Error in query execution: " . $stmt->error);
+        $rows_signin = signin_query($username, $password, "user");
+        if (empty($rows_signin) or $rows_signin[0]["username"] != $username) {
+            $query = "INSERT INTO `user` (`email`, `username`, `password`) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($query);
+    
+            if (!$stmt) {
+                die("Error in query preparation: " . $conn->error);
+            }
+    
+            $stmt->bind_param("sss", $email, $username, $password);
+            $result = $stmt->execute();
+    
+            if (!$result) {
+                die("Error in query execution: " . $stmt->error);
+            } else {
+                $response = array("success" => true, "message" => "sign up for {$email} and {$username} success");
+            }
         } else {
-            $response = array("success" => true, "message" => "Sign up success");
-            echo json_encode($response);
+            $response = array("success" => false, "message" => "user {$username} already exists");
         }
+        echo json_encode($response);
         mysqli_close($conn);
     }
 ?>
