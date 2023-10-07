ecbddad9a183d6221a26ccc64138952efb97b5e0
christodharma <13521009@std.stei.itb.ac.id>
2023-10-07 22:10:05 +0700

    fix: navbar to have more systematic html

--- client/js/navbar.js
+++ client/js/navbar.js
@@ -1,22 +1,24 @@
 addnavbar = () => {
     return document.getElementById("tabgroup").innerHTML =`
-        <a class="tabexpand" href="#">
-            <i class="fa-solid fa-bars"></i>
-        </a>
-        <img src="../image/logoregis.svg" alt="icon" width="10%" height="5%">
-        <div class="searchbar">
-            <input id="Searchinput" type="text" placeholder="Search..." value="" onkeypress="">
-            <button class="search" onclick="">
-                <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-        </div>
-        <a class="tabright" href="#">
-            <i class="fa-solid fa-cart-shopping"></i>
-            Cart
-        </a>
-        <a class="tabright" href="../pages/catalog.php">
-            <i class="fa-solid fa-clipboard-list"></i>
-            Catalog
-        </a>
+    <div class="navbar-container">
+    <a class="tabexpand" href="#">
+        <i class="fa-solid fa-bars"></i>
+    </a>
+    <img src="../image/logoregis.svg" alt="icon" width="10%" height="5%">
+    <div class="searchbar">
+        <input id="Searchinput" type="text" placeholder="Search..." value="" onkeypress="">
+        <button class="search" onclick="">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+    </div>
+    <a class="tabright" href="#">
+        <i class="fa-solid fa-cart-shopping"></i>
+        Cart
+    </a>
+    <a class="tabright" href="../pages/catalog.php">
+        <i class="fa-solid fa-clipboard-list"></i>
+        Catalog
+    </a>
+</div>
     `
 }
