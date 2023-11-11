6ce2f40c016a045452787631bd021aab3edaaf17
christodharma <13521009@std.stei.itb.ac.id>
2023-11-11 22:48:30 +0700

    fix: schema refactor

--- server/models/db.sql
+++ server/models/db.sql
@@ -1,11 +1,11 @@
 
-CREATE SCHEMA IF NOT EXISTS `saranghaengbok_db` DEFAULT CHARACTER SET utf8 ;
-USE `saranghaengbok_db` ;
+CREATE SCHEMA IF NOT EXISTS `saranghaengbok_php` DEFAULT CHARACTER SET utf8 ;
+USE `saranghaengbok_php` ;
 
 -- -----------------------------------------------------
--- Table `saranghaengbok_db`.`user`
+-- Table `saranghaengbok_php`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`user` (
+CREATE TABLE IF NOT EXISTS `saranghaengbok_php`.`user` (
   `email` VARCHAR(64) NOT NULL,
   `username` VARCHAR(45) NOT NULL,
   `password` VARCHAR(256) NOT NULL,
@@ -14,20 +14,20 @@ CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`user` (
 INSERT INTO `user` (`email`, `username`, `password`) VALUES ('christodharma@gmail.com', 'christodharma', 'yangliatinicumachristo');
 
 -- -----------------------------------------------------
--- Table `saranghaengbok_db`.`admin`
+-- Table `saranghaengbok_php`.`admin`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`admin` (
+CREATE TABLE IF NOT EXISTS `saranghaengbok_php`.`admin` (
   `admin_username` VARCHAR(45) NOT NULL,
   CONSTRAINT `fk_username`
     FOREIGN KEY (`admin_username`)
-    REFERENCES `saranghaengbok_db`.`user`(`username`)
+    REFERENCES `saranghaengbok_php`.`user`(`username`)
     ON UPDATE CASCADE
 );
 
 -- -----------------------------------------------------
--- Table `saranghaengbok_db`.`item`
+-- Table `saranghaengbok_php`.`item`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`item` (
+CREATE TABLE IF NOT EXISTS `saranghaengbok_php`.`item` (
   `item_id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `picture_path` VARCHAR(255) NOT NULL,
@@ -38,34 +38,34 @@ CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`item` (
   PRIMARY KEY (`item_id`, `Seller_username`),
   CONSTRAINT `fk_item_Seller1`
     FOREIGN KEY (`Seller_username`)
-    REFERENCES `saranghaengbok_db`.`user` (`username`)
+    REFERENCES `saranghaengbok_php`.`user` (`username`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 -- -----------------------------------------------------
--- Table `saranghaengbok_db`.`cart`
+-- Table `saranghaengbok_php`.`cart`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`cart` (
+CREATE TABLE IF NOT EXISTS `saranghaengbok_php`.`cart` (
   `item_id` INT NOT NULL,
   `cart_username` VARCHAR(45) NOT NULL,
   `item_quantity` INT NOT NULL,
   PRIMARY KEY(`item_id`, `cart_username`),
   CONSTRAINT `fk_cart_username`
     FOREIGN KEY (`cart_username`)
-    REFERENCES `saranghaengbok_db`.`user`(`username`)
+    REFERENCES `saranghaengbok_php`.`user`(`username`)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   CONSTRAINT `fk_cart_item_id`
     FOREIGN KEY (`item_id`)
-    REFERENCES `saranghaengbok_db`.`item`(`item_id`)
+    REFERENCES `saranghaengbok_php`.`item`(`item_id`)
     ON DELETE CASCADE
 );
 
 -- -----------------------------------------------------
--- Table `saranghaengbok_db`.`transaction`
+-- Table `saranghaengbok_php`.`transaction`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`transaction` (
+CREATE TABLE IF NOT EXISTS `saranghaengbok_php`.`transaction` (
   `item_id` INT NOT NULL,
   `buyer_username` VARCHAR(45) NOT NULL,
   `seller_username` VARCHAR(45) NOT NULL,
@@ -73,22 +73,22 @@ CREATE TABLE IF NOT EXISTS `saranghaengbok_db`.`transaction` (
   PRIMARY KEY(`item_id`, `buyer_username`, `seller_username`),
   CONSTRAINT `fk_item_id`
     FOREIGN KEY (`item_id`)
-    REFERENCES `saranghaengbok_db`.`item`(`item_id`)
+    REFERENCES `saranghaengbok_php`.`item`(`item_id`)
     ON UPDATE NO ACTION
     ON DELETE NO ACTION,
   CONSTRAINT `fk_buyer_username`
     FOREIGN KEY (`buyer_username`)
-    REFERENCES `saranghaengbok_db`.`user`(`username`)
+    REFERENCES `saranghaengbok_php`.`user`(`username`)
     ON UPDATE CASCADE,
   CONSTRAINT `fk_seller_username`
     FOREIGN KEY (`seller_username`)
-    REFERENCES `saranghaengbok_db`.`user`(`username`)
+    REFERENCES `saranghaengbok_php`.`user`(`username`)
     ON UPDATE CASCADE
 );
 
 
-CREATE user `saranghaengbok_db_admin` IDENTIFIED BY 'BOOMbitchgetouttheway';
+CREATE user `saranghaengbok_php_admin` IDENTIFIED BY 'BOOMbitchgetouttheway';
 
 flush privileges;
 
-GRANT ALL ON `saranghaengbok_db`.* TO `saranghaengbok_db_admin`;
+GRANT ALL ON `saranghaengbok_php`.* TO `saranghaengbok_php_admin`;
